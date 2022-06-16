b2a2f059c7c3fb581af73b6939bd92d176ca0180
emitsuwu <ccugwuegbulam@gmail.com>
2022-06-16 10:02:04 -0500

    nothing changed here

---
+++ pracFiles/pandas/numpyPractice.py
@@ -0,0 +1,30 @@
+# this is a practice file to get familiar with Numpy implementation into pandas
+
+import numpy as np
+
+# single dimensional array
+print("Singledimensional array: ")
+d = np.array([1, 2, 3, 4, 5])
+print(type(d))
+print(d)
+
+# multidimensional array
+print()
+print("-------------")
+print("Multidimensional array: ")
+dd = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(type(dd))
+print()
+print(dd)
+
+# print out the shape of the array, and the data type of the "dd" array
+print()
+print("The shape of both single and multidimensional arrays in that specific order: ")
+print("-------------")
+print(d.shape)
+print(dd.shape)
+print(dd.dtype)
+
+#
+print()
+print("-------------")
